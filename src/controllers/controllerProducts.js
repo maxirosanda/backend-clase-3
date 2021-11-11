e08fda2b504f4b04ca85d82e30bdb5d8f9d5cabe
maxirosanda <maxi_rosanda@hotmail.com>
2021-11-11 10:42:44 -0300

    primero

---
+++ src/controllers/controllerProducts.js
@@ -0,0 +1,42 @@
+
+let products = [
+
+]
+
+export const viewProducts = (req,res)=>{
+  res.status(200).render('products',{products:products})
+}
+
+export const view = (req,res)=>{
+  
+    res.status(200).render('editProducts',{productosparaver:products})
+  
+  }
+
+  export const create = (req,res)=>{
+    let product = req.body
+    product.id = Math.floor(Math.random()*10000000000)
+    products.push(req.body)
+    console.log(products)
+    res.status(200).redirect('/editproductos')
+
+  
+  }
+
+  export const del = (req,res) =>{
+    console.log(req.body)
+    products = products.filter(element => element.id != req.body.id)
+    res.status(200).redirect('/editproductos')
+
+  }
+
+  export const update = (req,res) =>{
+    
+    let product = products.find(element => element.id == req.body.id)
+    if(req.body.name) product.name = req.body.name
+    if(req.body.price) product.price=req.body.price
+    if(req.body.stock) product.stock = req.body.stock
+    res.status(200).redirect('/editproductos')
+
+  }
+
